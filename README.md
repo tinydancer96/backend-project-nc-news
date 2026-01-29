8d753caff29bc8b375bdaffeceffd10acd89bb02
tinydancer96 <mardgelleneri@gmail.com>
2026-01-29 13:33:48 +0000

    add seed.js file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+Creating .env files
+.env files are not pushed to github due to the sensitive information they may contain. To set one up so that the databases connect appropriately please perform the following:
+
+1. create .env.test file - this will connect to the nc_news_test file. In this file, please add PGDATABASE=nc_news_test.
+2. create .env.development file - this will connect to the nc_news file. In this file, please add PGDATABASE=nc_news.
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,54 @@
-const db = require("../connection")
+const db = require("../connection");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query(`DROP TABLE IF EXISTS comments;`)
+    .then(() => db.query(`DROP TABLE IF EXISTS articles;`))
+    .then(() => db.query(`DROP TABLE IF EXISTS users;`))
+    .then(() => db.query(`DROP TABLE IF EXISTS topics;`))
+    .then(() => {
+      return db.query(`
+        CREATE TABLE topics (
+          slug VARCHAR(40) PRIMARY KEY,
+          description VARCHAR(255),
+          img_url VARCHAR(1000)
+        );
+      `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE users (
+          username VARCHAR(40) PRIMARY KEY,
+          name VARCHAR(20),
+          avatar_url VARCHAR(1000)
+        );
+      `);
+    })
+    .then(() => {
+      return db.query(`
+          CREATE TABLE articles (
+          article_id SERIAL PRIMARY KEY,
+          title VARCHAR(40),
+          topic VARCHAR(40) REFERENCES topics(slug),
+          author VARCHAR(40) REFERENCES users(username),
+          body TEXT,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          votes INT DEFAULT 0,
+          article_img_url VARCHAR(1000)
+          );
+       `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE comments (
+          comment_id SERIAL PRIMARY KEY,
+          article_id INT NOT NULL REFERENCES articles(article_id),
+          body TEXT,
+          votes INT DEFAULT 0,
+          author VARCHAR(40) REFERENCES users(username),
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )`);
+    });
 };
+
 module.exports = seed;
