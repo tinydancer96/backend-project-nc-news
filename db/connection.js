7a7f9dd48975fed8867e94a543b824bbd72c012a
tinydancer96 <mardgelleneri@gmail.com>
2026-02-10 01:27:40 +0000

    updated connection.js to successfully connect to online server

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,12 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
+// const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 } else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
+  console.log("connection successful");
 }
 
 const config = {};
@@ -20,4 +20,4 @@ if (ENV === "production") {
 }
 module.exports = new Pool(config);
 
-module.exports = db; // do I need to make db = new Pool(config)
+// module.exports = db; // do I need to make db = new Pool(config)
